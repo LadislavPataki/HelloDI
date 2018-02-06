b9414f48a06252eb827b97052cd94308a1c4435e
Ladislav Pataki <ladislav.pataki@outlook.com>
2018-02-06 22:56:47 +0100

    Hello DI

---
+++ HelloDI.UnitTests/SalutationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace HelloDI.UnitTests
+{
+    [TestClass]
+    public class SalutationTest
+    {
+        [TestMethod]
+        public void ExclaimWillWriteCorrectMessageToMessageWriter()
+        {
+            var writeMock = new Mock<IMessageWriter>();
+            var sut = new Salutation(writeMock.Object);
+
+            sut.Exclaim();
+
+            writeMock.Verify(w => w.Write("Hello DI!"));
+        }
+    }
+}
---
+++ HelloDI/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+
+namespace HelloDI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //var typeName =
+            //    ConfigurationManager.AppSettings["messageWriter"];
+            //var type = Type.GetType(typeName, true);
+            //IMessageWriter writer = (IMessageWriter)Activator.CreateInstance(type);
+
+
+            IMessageWriter writer = new ConsoleMessageWriter();
+            IMessageWriter secureWriter = new DecoratedMessageWriter(writer);
+            var salutation = new Salutation(secureWriter);
+
+
+            //IMessageWriter writer = new ConsoleMessageWriter();
+            //var salutation = new Salutation(writer);
+
+            salutation.Exclaim();
+        }
+    }
+
+    public class Salutation
+    {
+        private readonly IMessageWriter _writer;
+
+        public Salutation(IMessageWriter writer)
+        {
+            if (writer == null)
+                throw new ArgumentNullException(nameof(writer));
+
+            _writer = writer;
+        }
+
+        public void Exclaim()
+        {
+            _writer.Write("Hello DI!");
+        }
+    }
+
+    public interface IMessageWriter
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleMessageWriter : IMessageWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class LateBindedConsoleMessageWriter : IMessageWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"{message} - late binding message writer");
+        }
+    }
+
+    public class DecoratedMessageWriter : IMessageWriter
+    {
+        private readonly IMessageWriter _writer;
+
+        public DecoratedMessageWriter(IMessageWriter writer)
+        {
+            if (writer == null)
+                throw new ArgumentNullException(nameof(writer));
+
+            _writer = writer;
+        }
+
+        public void Write(string message)
+        {
+            var decoratedMessage = $"{message} - decorated";
+                _writer.Write(decoratedMessage);
+        }
+    }
+}
+
+
+
+
+
